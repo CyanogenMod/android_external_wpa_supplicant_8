7b8da2ee6c87c38144dae7882a29d98c616a8738
Dmitry Shmidt <dimitrysh@google.com>
2011-07-24 17:12:33 -0700

    Fix incremental builds by adding Cleanspec step
    Change-Id: Id06cc40081a5ff33e015b7aeed8fffd73ef09721
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -52,3 +52,4 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/wpa_cli_intermediat
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/wpa_supplicant_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/wpa_supplicant_intermediates/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/wpa_cli_intermediates/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/wpa_supplicant_intermediates/*)
