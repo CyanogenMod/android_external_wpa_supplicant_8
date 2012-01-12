bf5edf439c90418b6f4122ff5e3925123263bda4
Irfan Sheriff <isheriff@google.com>
2012-01-11 16:54:57 -0800

    Add network connection event for status
    We need both supplicant state and network connection state
    for proper sync up with framework
    
    Bug: 5534039
    Change-Id: If5094b40f67271e03bc09682749ccd3fd043757b

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -928,6 +928,15 @@ static int wpa_supplicant_ctrl_iface_status(struct wpa_supplicant *wpa_s,
 		     "id=%d state=%d BSSID=" MACSTR,
 		     wpa_s->current_ssid ? wpa_s->current_ssid->id : -1,
 		     wpa_s->wpa_state, MAC2STR(wpa_s->pending_bssid));
+	if (wpa_s->wpa_state == WPA_COMPLETED) {
+		struct wpa_ssid *ssid = wpa_s->current_ssid;
+		wpa_msg_ctrl(wpa_s, MSG_INFO, WPA_EVENT_CONNECTED "- connection to "
+			MACSTR " completed %s [id=%d id_str=%s]",
+			MAC2STR(wpa_s->bssid), wpa_s->reassociated_connection ?
+			"(reauth)" : "(auth)",
+			ssid ? ssid->id : -1,
+			ssid && ssid->id_str ? ssid->id_str : "");
+	}
 #endif /* ANDROID */
 
 	return pos - buf;
