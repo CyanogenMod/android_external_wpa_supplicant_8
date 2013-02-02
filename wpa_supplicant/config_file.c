a526cad2b2713d368003f40c0a23e89c5ec3d4d2
Bruno Randolf <br1@einfach.org>
2013-02-02 11:10:33 +0000

    Save frequency to network config
    Also save frequency in wpa_config_write_network().
    
    This is necessary for creating IBSS networks which requires the
    frequency to be set.
    
    Change-Id: Ic051d1ca1ce3b1d4bad231d46e86c21e818af5de

--- wpa_supplicant/config_file.c
+++ wpa_supplicant/config_file.c
@@ -676,6 +676,7 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid)
 	INT_DEFe(fragment_size, DEFAULT_FRAGMENT_SIZE);
 #endif /* IEEE8021X_EAPOL */
 	INT(mode);
+	INT(frequency);
 	INT(proactive_key_caching);
 	INT(disabled);
 	INT(peerkey);
