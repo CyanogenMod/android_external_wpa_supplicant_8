b08a6362dede0f80638bc6a7984e595d60128d65
Sameer Thalappil <sameert@codeaurora.org>
2013-05-17 15:13:25 +0530

    Extension of use preferred channel list during GO creation
    Removing country argument from p2p_channel_to_freq function
    CRs-Fixed: 481409
    Git-commit: c6ccf12d3f6f35e295f9522dded535da8af6ab98
    Git-repo: git://w1.fi/srv/git/hostap.git
    Signed-hostap: Jouni Malinen <jouni@qca.qualcomm.com>
    
    Change-Id: I49f80dba8f7164ef50e4343e419c46d2338e75e9

--- src/p2p/p2p_utils.c
+++ src/p2p/p2p_utils.c
@@ -291,7 +291,6 @@ unsigned int p2p_get_pref_freq(struct p2p_data *p2p,
 	if (channels == NULL) {
 		if (p2p->cfg->num_pref_chan) {
 			freq = p2p_channel_to_freq(
-				p2p->cfg->country,
 				p2p->cfg->pref_chan[0].op_class,
 				p2p->cfg->pref_chan[0].chan);
 			if (freq < 0)
@@ -301,8 +300,7 @@ unsigned int p2p_get_pref_freq(struct p2p_data *p2p,
 	}
 
 	for (i = 0; p2p->cfg->pref_chan && i < p2p->cfg->num_pref_chan; i++) {
-		freq = p2p_channel_to_freq(p2p->cfg->country,
-					   p2p->cfg->pref_chan[i].op_class,
+		freq = p2p_channel_to_freq(p2p->cfg->pref_chan[i].op_class,
 					   p2p->cfg->pref_chan[i].chan);
 		if (p2p_channels_includes_freq(channels, freq))
 			return freq;
