65af1b08cb0d429d446af520472e20c026cd67aa
Dmitry Shmidt <dimitrysh@google.com>
2013-05-23 13:42:31 -0700

    Remove 'interface=iface' command prefix
    Change-Id: I01caeeff2cf356afb4ce0453d7a78f7ecfae1de6
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- wpa_supplicant/ctrl_iface_unix.c
+++ wpa_supplicant/ctrl_iface_unix.c
@@ -176,30 +176,6 @@ static void wpa_supplicant_ctrl_iface_receive(int sock, void *eloop_ctx,
 		else
 			reply_len = 2;
 	} else {
-#if defined(CONFIG_P2P) && defined(ANDROID_P2P)
-		char *ifname, *ifend;
-
-		ifname = os_strstr(buf, "interface=");
-		if (ifname != NULL) {
-			ifend = os_strchr(ifname + 10, ' ');
-			if (ifend != NULL)
-				*ifend++ = '\0';
-			else
-				*(ifname - 1) = '\0';
-			wpa_printf(MSG_DEBUG, "Found %s", ifname);
-			for (wpa_s = wpa_s->global->ifaces; wpa_s; wpa_s = wpa_s->next) {
-				if (os_strcmp(wpa_s->ifname, ifname + 10) == 0)
-					break;
-			}
-			if (wpa_s == NULL) {
-				wpa_printf(MSG_ERROR, "P2P: %s does not exist", ifname);
-				wpa_s = eloop_ctx;
-			}
-			if (ifend != NULL)
-				os_memmove(ifname, ifend, strlen(ifend) + 1);
-			wpa_printf(MSG_INFO, "wpa_s->ifname %s cmd %s", wpa_s ? wpa_s->ifname : "NULL", buf);
-		}
-#endif /* defined CONFIG_P2P && defined ANDROID_P2P */
 		reply = wpa_supplicant_ctrl_iface_process(wpa_s, buf,
 							  &reply_len);
 	}
