c89185f386408ee2201fc4f4fc50e950f894d4c3
Deepthi Gowri <c_gowri@qti.qualcomm.com>
2013-10-07 13:45:26 +0530

    P2P Extend postponing of concurrent scans for persistent GO
    Update the p2p_go_wait_client timestamp in p2p_go_configured() to
    address the case where the group is set up without the provisioning
    step.
    
    Change-Id: Idff6b55f2d572fc27ee8abcf05778a7ed5451632
    CRs-Fixed: 554201
    Git-commit: 6903ee6f62e4585451c1e4f90db8ca8d7cc82070
    Git-repo: git://w1.fi/srv/git/hostap.git
    Signed-hostap: Jouni Malinen <jouni@qca.qualcomm.com>

--- wpa_supplicant/p2p_supplicant.c
+++ wpa_supplicant/p2p_supplicant.c
@@ -894,6 +894,7 @@ static void p2p_go_configured(void *ctx, void *data)
 				"");
 		}
 
+		os_get_time(&wpa_s->global->p2p_go_wait_client);
 		if (params->persistent_group)
 			network_id = wpas_p2p_store_persistent_group(
 				wpa_s->parent, ssid,
