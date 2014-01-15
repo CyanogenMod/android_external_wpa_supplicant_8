6cd0b77e1e4f19d0ff916069b85f4bf191304933
Michael Gernoth <michael@gernoth.net>
2014-01-15 11:16:28 -0800

    capabilities ERANG error fix
    Add BOARD_WIFI_SKIP_CAPABILITIES flag to skip this portion of
    driver setup.  Looks like it's not supported in 3.0 and 3.4 kernel
    
    Change-Id: Id750f8f648a62e0c27a2c4597a3a9a46a349f95f

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -6757,7 +6757,9 @@ static int wpa_driver_nl80211_sta_add(void *priv,
 			   params->listen_interval);
 		NLA_PUT_U16(msg, NL80211_ATTR_STA_LISTEN_INTERVAL,
 			    params->listen_interval);
-	} else if (params->aid && (params->flags & WPA_STA_TDLS_PEER)) {
+	}
+#ifndef BOARD_WIFI_SKIP_CAPABILITIES
+	else if (params->aid && (params->flags & WPA_STA_TDLS_PEER)) {
 		wpa_printf(MSG_DEBUG, "  * peer_aid=%u", params->aid);
 		NLA_PUT_U16(msg, NL80211_ATTR_PEER_AID, params->aid);
 	}
@@ -6789,6 +6791,7 @@ static int wpa_driver_nl80211_sta_add(void *priv,
 			params->ext_capab_len, params->ext_capab);
 	}
 
+#endif
 	os_memset(&upd, 0, sizeof(upd));
 	upd.mask = sta_flags_nl80211(params->flags);
 	upd.set = upd.mask;
