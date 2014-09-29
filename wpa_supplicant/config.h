3604ec5e89d9f1a86dc58590b9a5ab67648cf6b4
Jouni Malinen <j@w1.fi>
2014-09-29 23:24:19 +0300

    Extend random MAC address support to allow OUI to be kept
    mac_addr=2 and preassoc_mac_addr=2 parameters can now be used to
    configure random MAC address to be generated by maintaining the OUI part
    of the permanent MAC address (but with locally administered bit set to
    1). Other than that, these values result in similar behavior with
    mac_addr=1 and preassoc_mac_addr=1, respectively.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>
    
    Change-Id: I20646bc5c4f3b9b7da344d96e80bb0df9e9cf306

--- wpa_supplicant/config.h
+++ wpa_supplicant/config.h
@@ -1070,6 +1070,7 @@ struct wpa_config {
 	 *
 	 * 0 = use permanent MAC address
 	 * 1 = use random MAC address for each ESS connection
+	 * 2 = like 1, but maintain OUI (with local admin bit set)
 	 *
 	 * By default, permanent MAC address is used unless policy is changed by
 	 * the per-network mac_addr parameter. Global mac_addr=1 can be used to
@@ -1087,6 +1088,7 @@ struct wpa_config {
 	 *
 	 * 0 = use permanent MAC address
 	 * 1 = use random MAC address
+	 * 2 = like 1, but maintain OUI (with local admin bit set)
 	 */
 	int preassoc_mac_addr;
 };
