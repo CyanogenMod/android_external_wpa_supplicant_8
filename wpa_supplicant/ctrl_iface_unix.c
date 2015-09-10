df368494deac4c9b3464b8d340726b6347c51aaa
Jouni Malinen <jouni@qca.qualcomm.com>
2015-09-10 11:19:44 +0300

    Drop some control interface debug print verbosity for send operations
    These prints were at DEBUG level (-d), but they can be very frequent, so
    drop them to MSGDUMP (-dd). This allows the prints to be suppressed in
    common debugging cases while still leaving them easily enablable to
    debug control interface issues without having to enable excessive
    debugging.
    
    CRs-Fixed: 909157
    Git-repo: git://w1.fi/srv/git/hostap.git
    Git-commit: 43fa110b0b6cc5f6db88497bcdbfae057bbe4d5b
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    
    Change-Id: Iabc8c86f048c9e484ef6260fbdaea70c697939c0

--- wpa_supplicant/ctrl_iface_unix.c
+++ wpa_supplicant/ctrl_iface_unix.c
@@ -717,7 +717,8 @@ static void wpa_supplicant_ctrl_iface_send(struct wpa_supplicant *wpa_s,
 		msg.msg_name = (void *) &dst->addr;
 		msg.msg_namelen = dst->addrlen;
 		if (sendmsg(sock, &msg, MSG_DONTWAIT) >= 0) {
-			wpa_printf(MSG_DEBUG, "CTRL_IFACE monitor sent successfully to %s",
+			wpa_printf(MSG_MSGDUMP,
+				   "CTRL_IFACE monitor sent successfully to %s",
 				   addr_txt);
 			dst->errors = 0;
 			continue;
