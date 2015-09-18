e97a2cebd268207a9918c3bbdc8660de51544ffd
Amarnath Hullur Subramanyam <amarnath@codeaurora.org>
2015-09-18 06:57:06 -0700

    Update AP WPA/RSN IE on all associations if driver can select BSS
    It is possible for driver-based BSS selection to end up reassociating
    back to the current AP. If wpa_supplicant preferred another BSS, it
    would have updated the internal knowledge of the AP's WPA/RSN IE when
    requesting a new connection. In the special case of existing association
    and new association being with the same BSS that is different from the
    wpa_supplicant preference, association event processing skipped the
    WPA/RSN IE update. This could result in the following 4-way handshake
    getting rejected due to incorrectly detected mismatch with AP's RSN/WPA
    IE between Beacon/Probe Response frame and EAPOL-Key msg 3/4.
    
    Fix this by updating the AP WPA/RSN IE on all association events when
    driver-based BSS selection is used regardless of whether the BSSID
    changes. This could also cover a theoretical case of the AP changing its
    RSN/WPA IE at the very moment we try to reassociate back to the same
    BSS.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    Change-Id: If37977900badf39603fad6c8ffadfe7d16e826ae
    Bug: 24110113

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -2043,6 +2043,7 @@ static void wpa_supplicant_event_assoc(struct wpa_supplicant *wpa_s,
 {
 	u8 bssid[ETH_ALEN];
 	int ft_completed;
+	int new_bss = 0;
 
 #ifdef CONFIG_AP
 	if (wpa_s->ap_iface) {
@@ -2074,6 +2075,7 @@ static void wpa_supplicant_event_assoc(struct wpa_supplicant *wpa_s,
 	if (os_memcmp(bssid, wpa_s->bssid, ETH_ALEN) != 0) {
 		wpa_dbg(wpa_s, MSG_DEBUG, "Associated to a new BSS: BSSID="
 			MACSTR, MAC2STR(bssid));
+		new_bss = 1;
 		random_add_randomness(bssid, ETH_ALEN);
 		os_memcpy(wpa_s->bssid, bssid, ETH_ALEN);
 		os_memset(wpa_s->pending_bssid, 0, ETH_ALEN);
@@ -2087,17 +2089,17 @@ static void wpa_supplicant_event_assoc(struct wpa_supplicant *wpa_s,
 				wpa_s, WLAN_REASON_DEAUTH_LEAVING);
 			return;
 		}
+	}
 
 #ifdef ANDROID
-		if (wpa_s->conf->ap_scan == 1) {
+	if (wpa_s->conf->ap_scan == 1) {
 #else
-		if (wpa_s->conf->ap_scan == 1 &&
-		    wpa_s->drv_flags & WPA_DRIVER_FLAGS_BSS_SELECTION) {
+	if (wpa_s->conf->ap_scan == 1 &&
+	    wpa_s->drv_flags & WPA_DRIVER_FLAGS_BSS_SELECTION) {
 #endif
-			if (wpa_supplicant_assoc_update_ie(wpa_s) < 0)
-				wpa_msg(wpa_s, MSG_WARNING,
-					"WPA/RSN IEs not updated");
-		}
+		if (wpa_supplicant_assoc_update_ie(wpa_s) < 0 && new_bss)
+			wpa_msg(wpa_s, MSG_WARNING,
+				"WPA/RSN IEs not updated");
 	}
 
 #ifdef CONFIG_SME
