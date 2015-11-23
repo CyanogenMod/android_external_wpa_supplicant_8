e86be60fcf99d03f85afcc39b717aa5bd169cd52
Rajkumar Manoharan <rmanohar@qti.qualcomm.com>
2015-11-23 18:40:26 +0530

    hostapd: Add Transmit Power Envelope IE when VHT is enabled
    Add Transmit Power Envelope element defined in IEEE P802.11-REVmc/D4.3,
    8.4.2.161.
    
    Git-commit: 3c417499e71b0c7f2dfae69fb1b67eb31e467ba6
    Git-repo : git://w1.fi/srv/git/hostap.git
    CRs-Fixed: 979946
    Change-Id: Ib3e11ee78cbe8e4d875d6c43dc2ec5539043fdb1
    Signed-off-by: Rajkumar Manoharan <rmanohar@qti.qualcomm.com>

--- src/ap/beacon.c
+++ src/ap/beacon.c
@@ -466,6 +466,7 @@ static u8 * hostapd_gen_probe_resp(struct hostapd_data *hapd,
 	if (hapd->iconf->ieee80211ac && !hapd->conf->disable_11ac) {
 		pos = hostapd_eid_vht_capabilities(hapd, pos);
 		pos = hostapd_eid_vht_operation(hapd, pos);
+		pos = hostapd_eid_txpower_envelope(hapd, pos);
 	}
 	if (hapd->conf->vendor_vht)
 		pos = hostapd_eid_vendor_vht(hapd, pos);
@@ -925,6 +926,7 @@ int ieee802_11_build_ap_params(struct hostapd_data *hapd,
 	if (hapd->iconf->ieee80211ac && !hapd->conf->disable_11ac) {
 		tailpos = hostapd_eid_vht_capabilities(hapd, tailpos);
 		tailpos = hostapd_eid_vht_operation(hapd, tailpos);
+		tailpos = hostapd_eid_txpower_envelope(hapd, tailpos);
 	}
 	if (hapd->conf->vendor_vht)
 		tailpos = hostapd_eid_vendor_vht(hapd, tailpos);
